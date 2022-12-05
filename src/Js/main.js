da4402bc8358d5aeefd21c54aaa0e44d86699084
Matheus Alencar <matlopes96@outlook.com>
2022-12-04 21:30:22 -0300

    adicionei interatividade com o contador de notificações e responsividade

---
+++ src/Js/main.js
@@ -0,0 +1,46 @@
+const notificationsCounter = document.querySelector(".notifications-counter");
+const markAllButton = document.querySelector(".mark-all-button");
+const notificationDots = document.querySelectorAll(".notification-dot");
+let notifications = document.querySelectorAll("main ul li");
+
+//Other method to work with the counter:
+// let notificationsCounterNumber = [...notifications].filter((notification) => notification.classList == "new-notification").length;
+// notificationsCounter.innerHTML = notificationsCounterNumber;
+
+notificationsCounterNumber = 0;
+
+for (let index = 0; index < notifications.length; index++) {
+    if (notifications[index].classList == "new-notification") {
+        notificationDots[index].style.display = "inline-block";
+
+        notificationsCounterNumber++;
+    }
+    
+    notificationsCounter.innerHTML = notificationsCounterNumber;
+
+    notifications[index].addEventListener("mouseover", 
+        () => {
+            if (notifications[index].classList == "new-notification") {
+                notifications[index].classList.remove("new-notification");
+
+                notificationDots[index].style.display = "none";
+
+                notificationsCounterNumber--;
+
+                notificationsCounter.innerHTML = notificationsCounterNumber;
+            }
+        }
+    )   
+}
+
+markAllButton.addEventListener("click",
+    () => {
+        notificationsCounter.innerHTML = 0;
+
+        for (let index = 0; index < notifications.length; index++) {
+            notifications[index].classList.remove("new-notification");
+            
+            notificationDots[index].style.display = "none";
+        }
+    }
+)
